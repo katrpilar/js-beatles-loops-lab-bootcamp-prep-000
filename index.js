a9d29c3ad599cb61a8004d9a5be825c8882fc283
Katrina Brinson <kbrinso@gmail.com>
2018-01-05 01:38:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 function theBeatlesPlay(musicians, instruments){
   var array = [];
   for (var i = 0; i < musicians.length; i++){
-    array.push(`${musicians} plays ${instruments}`)
+    var m = musicians[i];
+    var i = instruments[i];
+    array.push(`${m} plays ${i}`)
   }
   return array;
 }
