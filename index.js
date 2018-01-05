11f585d0f40c8f0abd3c0c67a8d5e26ffc0706f8
Katrina Brinson <kbrinso@gmail.com>
2018-01-05 02:31:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ function johnLennonFacts(array){
 function iLoveTheBeatles(n){
   var arry = [];
   do {
+    arry.push["I love the Beatles!"]
     n++
   } while(n < 15);
-  return 
+  return arry;
 }
